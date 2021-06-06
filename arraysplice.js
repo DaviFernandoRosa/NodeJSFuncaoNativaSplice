24696122f266cf44ee3c4b9f10c24286c9d9ba4b
Davi Fernando Rosa <davir0514@gmail.com>
2021-06-06 12:12:04 -0300

    Arquivo

---
+++ arraysplice.js
@@ -0,0 +1,15 @@
+
+
+
+
+// funcão Nativa para remover elemento no   array 
+
+const diasDaSemana = [
+    "domingo", "segunda-feira", "terça-feira",
+    "quarta-feira", "quinta-feira", "sexta-feira", "sábado"
+];
+
+diasDaSemana.splice(2,1);
+console.log(diasDaSemana);
+// removendo "terça-feira"
+
